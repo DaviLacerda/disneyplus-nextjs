08f6bb085e4a834b50c58fa8581cb0a5f56db95e
Davi Lacerda <dlacerda9@gmail.com>
2022-02-11 09:43:43 -0300

    change <a> for <Link>

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,6 @@
 import { StyledHeader } from "./styles";
 import { useEffect } from "react";
+import Link from "next/link";
 
 function Header() {
     function openOtherOptions() {
@@ -29,30 +30,30 @@ function Header() {
             <div className="left">
                 <div className="left__logo"></div>
 
-                <a href="/">
+                <Link href="/">
                     <div className="left__container">
                         <svg className="left__icon" viewBox="0 0 36 36"> 
                             <path d="M26.882 19.414v10.454h-5.974v-5.227h-5.974v5.227H8.961V19.414H5.227L17.921 6.72l12.694 12.694h-3.733z"></path>
                         </svg>
                         <p>Home</p>
                     </div>
-                </a>
+                </Link>
                 
-                <a href="/search">
+                <Link href="/search">
                     <div className="left__container">
                         <svg className="left__icon" viewBox="0 0 36 36">
                             <path d="M21.866 24.337c-3.933 2.762-9.398 2.386-12.914-1.13-3.936-3.936-3.936-10.318 0-14.255 3.937-3.936 10.32-3.936 14.256 0 3.327 3.327 3.842 8.402 1.545 12.27l4.56 4.558a2 2 0 0 1 0 2.829l-.174.173a2 2 0 0 1-2.828 0l-4.445-4.445zm-5.786-1.36a6.897 6.897 0 1 0 0-13.794 6.897 6.897 0 0 0 0 13.794z"></path>
                         </svg>
                         <p>Search</p>
                     </div>
-                </a>
+                </Link>
 
-                <a href="/watchlist">
+                <Link href="/watchlist">
                     <div className="left__container">
                         <span>+</span>
                         <p>Watchlist</p>
                     </div>
-                </a>
+                </Link>
 
                 <div className="left__container desktop">
                     <svg className="left__icon" viewBox="0 0 36 36">
@@ -83,10 +84,10 @@ function Header() {
             </div>
 
             <div className="right">
-                <a href="/contact">
+                <Link href="/contact">
                     <h2>Davi Lacerda</h2>
                     <div className="dropdown__avatar"></div>
-                </a>
+                </Link>
             </div>
         </StyledHeader>
     )
